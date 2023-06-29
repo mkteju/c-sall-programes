b145cd2fcc037a9bf025fe7d0e0a552ace407b94
tejashvini <tejashvini2912@gmail.com>
2023-06-29 17:16:51 +0530

    first commit

---
+++ WEEK.C
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<conio.H>
+void main()
+{
+int a;
+char no;
+printf("enter the value a");
+scanf("%d",&a);
+printf("enter the value choise");
+scanf("%d",&choise);
+switch(no)
+{
+case'1':
+{
+printf("sunday");
+break;
+}
+case'2':
+{
+printf("monday");
+break;
+}
+case'3':
+{
+printf("tuseday");
+break;
+}
+case'4':
+{
+printf("wednseday");
+break;
+}
+case'5':
+{
+printf("thusdau");
+break;
+}
+case'6':
+{
+printf("friday");
+break;
+}
+case'7':
+{
+printf("saturday");
+break;
+}
+default;
+{
+printf("wrong opetion");
+break;
+}
+getch();
+}
